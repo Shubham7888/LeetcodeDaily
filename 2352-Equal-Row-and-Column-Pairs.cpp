703375e8b69a9b45a981c7efe4d13ac89dc59676
Shubham yadav <87815979+Shubham7888@users.noreply.github.com>
2024-10-07 22:37:29 +0530

    [2352] [Time Beats: 5.04%] [Memory Beats: 100.00%] - LeetPush

---
+++ 2352-Equal-Row-and-Column-Pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        
+        int count=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                bool flag=true;
+                for(int k=0;k<grid.size();k++){
+                    if(grid[i][k]!=grid[k][j]){
+                        flag=false;
+                        break;
+                    }
+
+                }
+                if(flag)
+                count++;
+            }
+        }
+        return count;
+    }
+};
